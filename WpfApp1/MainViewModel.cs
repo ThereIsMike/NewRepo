81742f5a1ca5888713e9b6a660d4f46cdfbb593a
Michal Kozminski <kozminski.michal@gmail.com>
2017-11-10 22:29:25 +0000

    New items adding

--- WpfApp1/MainViewModel.cs
+++ WpfApp1/MainViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Reactive.Bindings;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +15,16 @@ namespace WpfApp1
         public ObservableCollection<ShoppingList> List { get; set; } = new ObservableCollection<ShoppingList>();
 
         public ObservableCollection<Buyers> UserList { get; set; } = new ObservableCollection<Buyers>();
+
+        public ReactiveProperty<bool> PushProduct { get; set; } = new ReactiveProperty<bool>();
+
+        public ReactiveProperty<bool> ProductUpdated { get; set; } = new ReactiveProperty<bool>();
+
+        public ReactiveProperty<string> ProductName { get; set; } = new ReactiveProperty<string>("Empty");
+
         public MainViewModel()
         {
+            Subscriptions();
             using (var db = new ShoppingContext())
             {
     
@@ -31,6 +41,27 @@ namespace WpfApp1
                 }
             }
             
+        }
+
+        void Subscriptions()
+        {
+            this.PushProduct.Subscribe(x => 
+            { if (x)
+                {
+                    Console.WriteLine(this.ProductName.Value.ToString());
+                    this.ProductName.Value = "";
+                    this.ProductUpdated.Value = true;
+                }
+            });
+
+            Observable.Interval(TimeSpan.FromSeconds(0.5)).Subscribe(_ =>
+            {
+                if (this.ProductUpdated.Value)
+                {
+                    this.PushProduct.Value = !this.PushProduct.Value;
+                    this.ProductUpdated.Value = false;
+                }
+            });
         }
     }
 }
